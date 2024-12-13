6b8c78149075df6a9f9d7d711dca2d4d13c6b03b
Mehrab <meh.raab77.brmn@gamil.com>
2024-12-13 14:36:48 +0330

    toggle visibility

--- src/components/OLComponent.jsx
+++ src/components/OLComponent.jsx
@@ -6,44 +6,51 @@ import { useEffect } from "react";
 import { useRef } from "react";
 import { Layer } from "iconsax-react";
 import "./OLComponent.css"
+import { useState } from "react";
 
 function OLComponent() {
     const mapRef = useRef(null);
+    const [visible, setVisible] = useState(false)
 
+    const wmsLayers = [
+        new TileLayer({
+            name: "ostan",
+            source: new TileWMS({
+                url : "http://localhost:8585/geoserver/webgis/wms",
+                params: {
+                    LAYERS: "webgis:ostan",
+                    TILED: true,
+                    FORMAT: "image/png"
+                },
+                serverType: "geoserver",
+                crossOrigin: "ananymous"
+            }),
+            visible: true
+        }),
+        new TileLayer({
+            name : "states",
+            source : new TileWMS({
+                url:"http://localhost:8585/geoserver/topp/wms",
+                params: {
+                    LAYERS: "topp:states",
+                    TILED: true,
+                    FORMAT: "image/png"
+                },
+                serverType: "geoserver",
+                crossOrigin: "ananymous"
+            }),
+            visible: true
+        })
+    ]
     useEffect(()=>{
         if(!mapRef.current) return;
 
-        const wmsLayers = [
-            new TileLayer({
-                source: new OSM()
-            }),
-            new TileLayer({
-                source: new TileWMS({
-                    url : "http://localhost:8585/geoserver/webgis/wms",
-                    params: {
-                        LAYERS: "webgis:ostan",
-                        TILED: true,
-                        FORMAT: "image/png"
-                    },
-                    serverType: "geoserver",
-                    crossOrigin: "ananymous"
-                }),
-            }),
-            new TileLayer({
-                source : new TileWMS({
-                    url:"http://localhost:8585/geoserver/topp/wms",
-                    params: {
-                        LAYERS: "topp:states",
-                        TILED: true,
-                        FORMAT: "image/png"
-                    },
-                    serverType: "geoserver",
-                    crossOrigin: "ananymous"
-                }) 
-            })
-        ]
-
         const map = new Map({
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                })
+            ],
             target: mapRef.current,
             view: new View({
                 center: fromLonLat([51.389, 35.6892]),
@@ -60,11 +67,34 @@ function OLComponent() {
         }
     }, [])
 
+    const ToggleLayerSwitch = (layer)=>{
+        const newVisibility = !layer.get("visible");
+        layer.setVisible(newVisibility);
+    }
+
     return (
         <>
         <div className="layer-switcher">
             <Layer size="28" color="#FF8A65"/>
-            <div className="layer-switcher__content" />
+            <div className="layer-switcher__content">
+                {wmsLayers.length > 0 && (
+                    wmsLayers.map((lyr, index)=>{
+                        return(
+                            <div key={index}>
+                                <label style={{color: "black"}}>
+                                    <input 
+                                        type={"checkbox"}
+                                        id={lyr.get("name")}
+                                        checked={lyr.get("visible")}
+                                        onChange = {()=> ToggleLayerSwitch(lyr)}
+                                    />
+                                    {lyr.get("name")}
+                                </label>
+                            </div>
+                        )
+                    })
+                )}
+            </div>
         </div>
         <div ref={mapRef} style={{width: "100%", height: "100vh"}} tabIndex={0}></div>
         </>
